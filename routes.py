4b5f8a03d482163ecb803018a3abb771570b1d71
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-10 11:20:28 +1200

    New pages .html made

--- routes.py
+++ routes.py
@@ -10,6 +10,18 @@ app = Flask(__name__)
 def home():     # ROUTE FUNCTION
     return render_template("home.html")
 
+@app.route("/planes")  # ROUTE DECORATOR
+def planes():     # ROUTE FUNCTION
+    return render_template("planes.html")
+
+@app.route("/engines")  # ROUTE DECORATOR
+def engines():     # ROUTE FUNCTION
+    return render_template("engines.html")
+
+@app.route("/create")  # ROUTE DECORATOR
+def create():     # ROUTE FUNCTION
+    return render_template("create.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)  # live updates code when building a website
